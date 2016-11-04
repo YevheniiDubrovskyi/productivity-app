f69843f4df909a15920a55f2e43dc3b387fd20b5
Yevhenii Dubrovskyi <yevhenii_dubrovskyi@epam.com>
2016-11-04 02:24:08 +0200

    Done timer and almost done logic part for reports

--- productivity-app/gulpfile.js
+++ productivity-app/gulpfile.js
@@ -7,28 +7,34 @@ gulp.task('default', ['connect',
 
 gulp.task('connect', () => {
   connect.server({
-    root: '.',
+    root: './src',
     livereload: true,
     port: 8888
   });
 });
 
 gulp.task('watch', () => {
-  gulp.watch('./css/*.css', ['css']);
-  gulp.watch('*.html', ['html']);
+  gulp.watch('./src/css/*.css', ['css']);
+  gulp.watch('./src/js/*.js', ['js']);
+  gulp.watch('./src/*.html', ['html']);
 });
 
-gulp.task('css', () => 
-  gulp.src('./css/*.css')
+gulp.task('css', () =>
+  gulp.src('./src/css/*.css')
       .pipe(autoprefixer({
         cascade: false,
         browsers: ['ie 6-8', 'last 10 versions']
       }))
-      .pipe(gulp.dest('./css'))
+      .pipe(gulp.dest('./src/css'))
+      .pipe(connect.reload())
+);
+
+gulp.task('js', () =>
+  gulp.src('./src/js/*.js')
       .pipe(connect.reload())
 );
 
 gulp.task('html', () =>
-  gulp.src('*.html')
+  gulp.src('./src/*.html')
       .pipe(connect.reload())
 );
